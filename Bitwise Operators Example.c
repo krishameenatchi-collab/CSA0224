d0a51b8e8c1f923bba8343701c0d110e325073d9
krishameenatchi-collab <krishameenatchi@gmail.com>
2026-02-17 06:35:37 +0530

    Create Bitwise Operators Example.c

---
+++ Bitwise Operators Example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+
+    printf("a & b = %d\n", a & b);
+    printf("a | b = %d\n", a | b);
+    printf("a ^ b = %d\n", a ^ b);
+    printf("a << 1 = %d\n", a << 1);
+    printf("a >> 1 = %d\n", a >> 1);
+
+    return 0;
+}
