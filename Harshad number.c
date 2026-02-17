36d35634b70d9b552000d845492f3853a1b177b5
Krisha <krishameenatchi@gmail.com>
2026-02-17 20:49:07 +0530

    Create Harshad number.c

---
+++ Harshad number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, temp, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        sum += digit;
+        temp /= 10;
+    }
+
+    if (num % sum == 0)
+        printf("Harshad Number\n");
+    else
+        printf("Not a Harshad Number\n");
+
+    return 0;
+}
