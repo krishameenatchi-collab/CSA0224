3c33738e3cc1b1c019757fa390d3d4d7183b8638
Krisha <krishameenatchi@gmail.com>
2026-03-04 07:41:18 +0530

    Create Move Zeros to End.c

---
+++ Move Zeros to End.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {0,1,9,8,0,2};
+    int n = 6, j = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
