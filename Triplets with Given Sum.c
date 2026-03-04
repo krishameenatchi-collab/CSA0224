64a00b69b4e248d36e8d614a076584908a843c68
Krisha <krishameenatchi@gmail.com>
2026-03-04 07:46:54 +0530

    Create Triplets with Given Sum.c

---
+++ Triplets with Given Sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int n = 5, target = 9;
+
+    for(int i = 0; i < n-2; i++)
+        for(int j = i+1; j < n-1; j++)
+            for(int k = j+1; k < n; k++)
+                if(arr[i] + arr[j] + arr[k] == target)
+                    printf("%d %d %d\n",
+                           arr[i], arr[j], arr[k]);
+
+    return 0;
+}
